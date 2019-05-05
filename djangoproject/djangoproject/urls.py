62ec8d0d8991e348f55c734b1e596768276da771
asaleem <arsalan_saleem@mentor.com>
2019-05-05 16:20:57 +0500

    sparsh theme added

--- djangoproject/djangoproject/urls.py
+++ djangoproject/djangoproject/urls.py
@@ -20,18 +20,18 @@ from django.conf.urls import url
 
 
 urlpatterns = [
+
     url(r'^$', include('landing.urls')),
     url(r'^sk/',include('sk.urls')),
-
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
 
 ]
-
-
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 # if settings.DEBUG:
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
