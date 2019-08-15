c25dbfe8d4570a3a092bd5abc675d8b0f86e4a63
Arsalan <Arsalan914@gmail.com>
2019-08-15 10:02:30 +0500

    little tweaks to revise djano

--- djangoproject/posts/urls.py
+++ djangoproject/posts/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = "posts-app"
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^details/(?P<id>\d+)/$', views.details, name='details'),
